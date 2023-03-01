670a22b6fdfb2f02e337a433f5f6f21712fb6754
Omar630603 <omar.yem1111@gmail.com>
2023-03-01 23:40:05 +0700

    Add views

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const productRoutes = require("./routes/product.routes");
+const apiProductRoutes = require("./routes/api/product.routes");
+const webProductRoutes = require("./routes/web/products.routes");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/styles", express.static(path.join(__dirname, "web", "styles")));
+app.set("views", path.join(__dirname, "web", "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
+  try {
+    if (req.query.message) {
+      return res.render("index", { message: req.query.message });
+    } else {
+      return res.render("index");
+    }
+  } catch (err) {
+    return res.render("error", err);
+  }
+});
+
+app.use("/", webProductRoutes);
+
+app.get("/api/v1/test", (req, res) => {
   if (req.body.message) {
     res.status(200).json({ message: req.body.message });
   } else {
@@ -13,6 +33,6 @@ app.get("/", (req, res) => {
   }
 });
 
-app.use("/api/v1", productRoutes);
+app.use("/api/v1", apiProductRoutes);
 
 module.exports = app;
--- routes/api/product.routes.js
+++ routes/api/product.routes.js
@@ -6,7 +6,7 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
-} = require("../controllers/product.controller");
+} = require("../../controllers/api/product.controller");
 
 const router = express.Router();
 
